903a2e520e93a87f69be8546f3e7c1d2be55184d
fanti08 <stete.fanti@gmail.com>
2022-05-03 19:50:18 +0200

    BUG FIXES

--- MDG 2D/Assets/Scripts/APPS/Calculator/calcu.cs
+++ MDG 2D/Assets/Scripts/APPS/Calculator/calcu.cs
@@ -2,13 +2,13 @@
 using TMPro;
 using System;
 using System.Globalization;
+using System.Linq;
 
 public class calcu : MonoBehaviour
 {
     [SerializeField] private float result;
     [SerializeField] private float saved;
     [SerializeField] public TextMeshProUGUI txt_result;
-    [SerializeField] private float multiplier = 1;
     [SerializeField] private string theOperation;
     [SerializeField] public TMP_FontAsset LiberationSans;
     [SerializeField] public TMP_FontAsset Junegull;
@@ -26,50 +26,40 @@ public class calcu : MonoBehaviour
             txt_result.font = LiberationSans;
             txt_result.fontStyle = FontStyles.Bold;
         }
+
+        print(result);
     }
 
+    float multiplier()
+    {
+        int lenght = txt_result.text.Split('.').Count() > 1 ? txt_result.text.Substring(txt_result.text.IndexOf(".")).Length : 0;
+        float mp = 1;
+        for (int i = lenght; i > 0; i--) mp *= .1f;
+        return mp;
+    }
+    
     public void period()
     {
-        if (multiplier > .2)
+        if (multiplier() > .2)
         {
-            multiplier = .1f;
-            txt_result.text = result.ToString("F1", CultureInfo.InvariantCulture);/*/
-            if (txt_result.text != "+" && txt_result.text != "-" && txt_result.text != "x" && txt_result.text != "÷" && txt_result.text != "√" && txt_result.text != "^") txt_result.text += ".";
-            else
-            {
-                txt_result.text = result.ToString("F2", CultureInfo.InvariantCulture);
-                txt_result.text += ".";
-            }/*/
+            txt_result.text = result.ToString("G", CultureInfo.InvariantCulture);
+            txt_result.text = result.ToString("F1", CultureInfo.InvariantCulture);
+            txt_result.text = txt_result.text.Substring(0, txt_result.text.Length - 1);
         }
-    }
-
-    public void zero()
-    {
-        if (multiplier < .2) txt_result.text += "0";
     }
 
     public void numbers(int num)
     {
-        if (multiplier > .2)
-        {
-            result *= 10;
-            result += num;
-        }
+        txt_result.text += num.ToString();
+        result = float.Parse(txt_result.text);
 
-        else
-        {
-            result += num * multiplier;
-            multiplier *= .1f;
-        }
-
-        if (num != 0 || multiplier > .2) txt_result.text = result.ToString();
+        txt_result.text = result.ToString("G", CultureInfo.InvariantCulture);
     }
 
     public void operation(string op)
     {
         theOperation = op;
         if (result != 0) saved = result;
-        multiplier = 1;
         result = 0;
         txt_result.text = theOperation;
     }
@@ -80,7 +70,7 @@ public class calcu : MonoBehaviour
         if (s_r == "r")
         {
             result = storedNum;
-            txt_result.text = storedNum.ToString();
+            txt_result.text = result.ToString("G", CultureInfo.InvariantCulture);
         }
     }
 
@@ -108,10 +98,10 @@ public class calcu : MonoBehaviour
                 break;
         }
         
-        txt_result.text = result.ToString();
+        txt_result.text = result.ToString("G", CultureInfo.InvariantCulture);
+        multiplier().Equals(1);
+        saved = result;
         result = 0;
-        saved = float.Parse(txt_result.text);
-        multiplier = 1;
     }
 
     public void delete()
@@ -125,9 +115,15 @@ public class calcu : MonoBehaviour
                 saved = 0;
             }
 
-            if (multiplier < .2) multiplier *= 10;
-
             result = float.Parse(txt_result.text);
+            print(theOperation);
+        }
+
+        else
+        {
+            theOperation = "";
+            result = saved;
+            txt_result.text = result.ToString("G", CultureInfo.InvariantCulture);
         }
     }
 }
